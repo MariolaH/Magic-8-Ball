56f894bbcc0cabdf152e22b6d0d68715db0d76e6
Mariola <123384146+MariolaH@users.noreply.github.com>
2023-09-11 12:33:05 -0400

    added comments

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet var eightBallImage: UIImageView!
 
     @IBAction func startButton(_ sender: UIButton) {
+         //create an array of images
         let imageOfEightBall = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+         //whenever the GO button is pressed it randomly displays an image
         eightBallImage.image = UIImage(named: imageOfEightBall[Int.random(in: 0...4)])
     }
     
